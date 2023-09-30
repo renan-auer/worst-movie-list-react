220d0714bad61e18918ba17fed75c84dce5e666f
Renan <renan.auer@gmail.com>
2023-09-29 23:16:51 -0300

    Create Layout

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="movies" element={<Movie />} />
+          <Route path="/movie-list" element={<Movie />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -3,20 +3,30 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/movies">Movies</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Outlet />
+      <div class="container">
+        <div class="sidebar">
+          <ul>
+            <li>
+              <Link to="/">
+                <i class="fa fa-home"></i>Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link to="/movie-list">
+                <i class="fa fa-list"></i>List
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div class="content">
+          <div class="header">Frontend React Test</div>
+          <div class="info">
+            <Outlet />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 };
 
 export default Layout;
